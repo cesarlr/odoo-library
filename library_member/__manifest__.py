1f9a0c4de203502758660a7f7bd26f9a6cc23c48
César López Ramírez <cesarlr@gmail.com>
2024-01-26 09:37:42 +0100

    Book List Template Extension

--- library_member/__manifest__.py
+++ library_member/__manifest__.py
@@ -10,6 +10,7 @@
         "security/library_security.xml",
         "views/book_view.xml",
         "views/library_menu.xml",
-        "views/member_view.xml"
+        "views/member_view.xml",
+        "views/book_list_template.xml",
     ],
 }
