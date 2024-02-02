d64cc4eb95fb5590f734c8b0c42521906a9b7e49
César López Ramírez <cesarlr@gmail.com>
2024-02-02 17:47:44 +0100

    Added publisher_country_id

--- library_app/models/library_book.py
+++ library_app/models/library_book.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
 class Book(models.Model):
@@ -30,7 +30,41 @@ class Book(models.Model):
     # Relational fields
     publisher_id = fields.Many2one("res.partner", string="Publisher")
     author_ids = fields.Many2many("res.partner", string="Authors")
-    
+    publisher_country_id = fields.Many2one(
+            'res.country', 'Publisher Country',
+            compute='_compute_publisher_country',
+            inverse='_inverse_publisher_country',
+            search='_search_publisher_country',
+    )
+    _sql_constraints = [
+            ("library_book_name_date_uq",
+             "UNIQUE (name,date_published)",
+             "Title and publication date must be unique."),
+            ("library_book_check_date",
+             "CHECK (date_published <= current_date)",
+             "Publication date must not be in the future."),
+            ]
+
+    @api.constrains("isbn")
+    def _constrain_isbn_valid(self):
+        for book in self:
+            if book.isbn and not book._check_isbn():
+                raise ValidationError("%s is an invalid ISBN" % book.isbn)
+
+    @api.depends("publisher_id.country_id")
+    def _compute_publisher_country(self):
+        for book in self:
+            book.publisher_country_id = book.publisher_id.country_id
+
+    def _inverse_publisher_country(self):
+        for book in self:
+            book.publisher_id.country_id = book.publisher_country_id
+
+    def _search_publisher_country(self, operator, value):
+        return [
+                ('publisher_id.country_id', operator, value)
+                ]
+
     def _check_isbn(self):
         self.ensure_one()
         ponderations = [1, 3] * 6
